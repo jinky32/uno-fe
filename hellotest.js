95b4a0e455c6f6a0ff41b30e1ecb90efc8bb50cd
jinky32 <jinky32@gmail.com>
2016-06-30 15:53:02 +0100

    attempt at rendering drupal json

--- hellotest.js
+++ hellotest.js
@@ -2,12 +2,30 @@ var express = require('express');
 var app = express();
 var routes = require('./routes/index');
 var request = require('request');
+var blogsurlall = "http://www.pickingorganic.org/blogsexportall";
 
 app.set('view engine','ejs');
 
 var server = app.listen (2000, function(){
     console.log('Waiting for you on port 2000');
 });
+
+request({
+    url:blogsurlall,
+    json:true
+}, function(error, response, body){
+    if (!error && response.statusCode===200) {
+        blogsdata_all = body;
+    }
+
+    var blogs = [];
+
+    blogsdata_all.blogs.forEach(function(item){
+        blogs = blogs.concat(item);
+    });
+
+    app.locals.blogsdata = blogs;
+})
 
 app.use('/', routes);
 
